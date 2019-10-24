4b2d453fbeefa699466254b1d3c5b6c8535ebea9
Vilela272 <guilherme.moreira@aluno.faculdadeimpacta.com.br>
2019-10-24 20:47:03 -0300

    Arrumando o test

--- com/faculdadeimpacta/calculadora/test/test_app_web_start.py
+++ com/faculdadeimpacta/calculadora/test/test_app_web_start.py
@@ -10,4 +10,4 @@ def test_root_status():
 def test_root_url():
     instancia_app = app_web_start.app.test_client()
     response = instancia_app.get('/devops_ac8')
-    assert response.data.decode('utf-8') == 'Index Page', 'Deveria existir essa rota'
+    assert response.data.decode('utf-8') == 'Index Page!', 'Deveria existir essa rota'
