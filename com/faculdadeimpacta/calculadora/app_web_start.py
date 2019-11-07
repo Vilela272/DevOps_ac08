fd31b5468178c76dbb59c9a6f3178a4e7d00adcd
Guilherme Vilela Moreira <53922433+Vilela272@users.noreply.github.com>
2019-11-07 20:34:24 -0300

    Update app_web_start.py

--- com/faculdadeimpacta/calculadora/app_web_start.py
+++ com/faculdadeimpacta/calculadora/app_web_start.py
@@ -4,11 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-app.run(host='0.0.0.0')
 @app.route('/devops_ac8')
 def index():
     return 'Boa noite!, Passei direto'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
